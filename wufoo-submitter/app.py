ef4f78bd72cbc31a57a2309796f5ee416a9b7f4e
Rob Mathers <robmathers@gmail.com>
2018-05-07 11:24:30 -0400

    Support multi-part and CORS

--- wufoo-submitter/app.py
+++ wufoo-submitter/app.py
@@ -8,7 +8,7 @@ app = Chalice(app_name='wufoo-submitter')
 def index():
     return ''
 
-@app.route('/submit/{form_id}', methods=['POST'], content_types=['application/x-www-form-urlencoded'])
+@app.route('/submit/{form_id}', methods=['POST'], content_types=['application/x-www-form-urlencoded', 'multipart/form-data'], cors=True)
 def submit(form_id):
     url = 'https://{subdomain}.wufoo.com/api/v3/forms/{form_id}/entries.json'
     subdomain = getenv('WUFOO_SUBDOMAIN')
