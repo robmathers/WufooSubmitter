71e6e05862a2d096511c424577caaab179b47365
Rob Mathers <robmathers@gmail.com>
2018-05-06 23:59:01 -0400

    Get Wufoo subdomain from environment variables

--- wufoo-submitter/app.py
+++ wufoo-submitter/app.py
@@ -1,8 +1,17 @@
-from chalice import Chalice
+from os import getenv
+from chalice import Chalice, BadRequestError, Response
 
 app = Chalice(app_name='wufoo-submitter')
 
 @app.route('/')
 def index():
     return ''
+
+@app.route('/submit/{form_id}', methods=['POST'], content_types=['application/x-www-form-urlencoded'])
+def submit(form_id):
+    url = 'https://{subdomain}.wufoo.com/api/v3/forms/{form_id}/entries.json'
+    subdomain = getenv('WUFOO_SUBDOMAIN')
+
+    if not subdomain:
+        raise BadRequestError('Wufoo subdomain has not been configured.')
 
